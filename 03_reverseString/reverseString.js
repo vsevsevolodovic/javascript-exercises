d3f7439a6b4da38be95aaaa6c05a0f424a8f1b19
Burak Bandas <124821125+vsevsevolodovic@users.noreply.github.com>
2023-03-14 18:48:15 +0300

    exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(someString) {
+    const stringList = someString.split("");
+    const listLen = stringList.length;
+    let newWord = "";
+    for (i = listLen - 1; i >= 0; i--) {
+        newWord += stringList[i];
+    }
+    return newWord;
 };
 
 // Do not edit below this line
